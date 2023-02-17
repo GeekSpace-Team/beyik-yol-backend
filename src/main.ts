ccfcffc42e0fb2e1c8baad2422affb078aeba544
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-02-17 20:30:54 +0500

    changed-port

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5500);
+  await app.listen(5501);
 }
 bootstrap();
