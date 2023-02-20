b226086e47eb85e960fff8f2a31e6201c05bbbb7
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-02-20 23:51:47 +0500

    ads-7

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5503);
+  await app.listen(6967);
 }
 bootstrap();
